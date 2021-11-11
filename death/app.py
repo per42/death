fb4ec1ca5306186a9951a90c6c96b57dd34d2cf0
Per Rosengren <per.rosengren@gmail.com>
2021-11-11 16:30:06 +0100

    Made axes logarithmic

--- death/app.py
+++ death/app.py
@@ -1,6 +1,8 @@
 from os import environ
 from io import BytesIO
+from datetime import datetime, timedelta
 
+import numpy as np
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -25,6 +27,14 @@ app = dash.Dash(
 server = app.server
 cache.init_app(server)
 
+now = datetime.now()
+
+
+def to_axis(x):
+    if isinstance(x, list):
+        x = np.asarray(x)
+    return (now + timedelta(days=90) - x) / timedelta(seconds=1)
+
 
 def serve_layout():
     data = {
@@ -48,7 +58,24 @@ def serve_layout():
     fig = go.Figure()
 
     for d in data.values():
-        fig.add_scatter(x=d["df"].index, y=d["df"], name=d["name"], mode=d["mode"])
+        fig.add_scatter(
+            x=to_axis(d["df"].index),
+            hovertemplate="%{text|%x}: %{y}",
+            text=d["df"].index,
+            y=d["df"],
+            name=d["name"],
+            mode=d["mode"],
+        )
+
+    ticks = []
+
+    ticks += [datetime(2022, m, 1) for m in range(12, 0, -1)]
+    ticks += [datetime(2021, m, 1) for m in range(12, 0, -1)]
+    ticks += [datetime(2020, m, 1) for m in range(10, 0, -3)]
+    ticks += [datetime(y, 1, 1) for y in range(2019, 2015, -1)]
+    ticks += [datetime(y, 1, 1) for y in range(2015, 1990, -5)]
+    ticks += [datetime(y, 1, 1) for y in range(1990, 1950, -10)]
+    ticks += [datetime(y, 1, 1) for y in range(1950, 1748, -50)]
 
     fig.update_layout(
         {
@@ -57,8 +84,17 @@ def serve_layout():
                 "title": "Andel som dött",
                 "tickformat": ",.2%",
                 "rangemode": "tozero",
+                "type": "log",
             },
-            "xaxis": {"title": "Året som löpte fram till datum"},
+            "xaxis": {
+                "title": "Året som löpte fram till datum",
+                "type": "log",
+                "autorange": "reversed",
+                "ticktext": [
+                    f"{tick:%Y}" if tick.month == 1 else f"{tick:%b}" for tick in ticks
+                ],
+                "tickvals": to_axis(ticks),
+            },
             "legend": {"yanchor": "top", "y": 0.99, "xanchor": "left", "x": 0.01},
             "margin": {"l": 10, "r": 10, "t": 80, "b": 10},
         }
